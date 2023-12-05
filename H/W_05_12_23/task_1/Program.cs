b0e9316fee15ce27f180813a3056e9070a0751f3
Kirill <borisenkobak@yandex.ru>
2023-12-05 18:13:00 +1000

    Task1Done

--- H/W_05_12_23/task_1/Program.cs
+++ H/W_05_12_23/task_1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿//Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+using System.ComponentModel;
+
+int n = int.Parse(Console.ReadLine()!);
+
+if (n % 7 == 0)
+{
+    if (n % 23 == 0)
+        Console.WriteLine("Найденно число!");
+    else
+        Console.WriteLine("Число не найденно");
+
+}
+
+else
+    Console.WriteLine("Число не найденно");
