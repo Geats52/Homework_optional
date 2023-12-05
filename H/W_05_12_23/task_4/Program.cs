5e598c41e400f08407a867cb4eeb25cb317cd581
Kirill <borisenkobak@yandex.ru>
2023-12-05 19:12:51 +1000

    Task4Done

---
+++ H/W_05_12_23/task_4/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+int N = int.Parse(Console.ReadLine()!);
+float result =  (float) N/100;
+Console.WriteLine(result);
