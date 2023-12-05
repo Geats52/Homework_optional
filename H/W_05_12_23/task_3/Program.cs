3386f047a098368308a39a294dbadee4c2165ff8
Kirill <borisenkobak@yandex.ru>
2023-12-05 19:00:48 +1000

    Task3Done

---
+++ H/W_05_12_23/task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int a = new Random().Next(10,99);
+Console.WriteLine(a);
+int b = new Random().Next(10,99);
+Console.WriteLine(b);
+
+if(a>b)
+	Console.WriteLine("Число 'a' большее");
+else
+	Console.WriteLine("Число 'b' большее");
+
+
+
